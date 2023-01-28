08380390b6bf5c7972baac7f70dd17ed28fc468c
roman <rm@indrive-tech.com>
2023-01-28 22:52:45 +0300

    add libs,init state

---
+++ src/services/reducers/ingredients.js
@@ -0,0 +1,30 @@
+const initialState = {
+  ingredients: [],
+  ingredientsRequest: false,
+  ingredientsFailed: false,
+  burgerIngredients: [],
+  currentIngredient: {},
+  order: {},
+};
+
+export const ingredientsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_INGREDIENTS_REQUEST: {
+      return {
+        ...state,
+        itemsRequest: true,
+      };
+    }
+    case GET_INGREDIENTS_SUCCESS: {
+      return {
+        ...state,
+        itemsFailed: false,
+        items: action.items,
+        itemsRequest: false,
+      };
+    }
+    case GET_INGREDIENTS_FAILED: {
+      return { ...state, itemsFailed: true, itemsRequest: false };
+    }
+  }
+};
