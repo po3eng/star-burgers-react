7b76de8df2f1b69e56b308dec0d9424aea6f9246
roman <rm@indrive-tech.com>
2023-03-08 13:21:43 +0300

    fix auth bugs

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -28,7 +28,6 @@ export function setCookie(name: string, value: string, props?: TCokieProps) {
   document.cookie = updatedCookie;
 }
 export function removeCookie(name: string) {
-  console.log(name);
   document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
 export function getCookie(name: string) {
