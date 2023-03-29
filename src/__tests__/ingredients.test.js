958364e7f5a1d93c567b18d46a3752a2bdcbd16a
roman <rm@indrive-tech.com>
2023-03-29 23:46:12 +0300

    test ingredient reducer

--- src/__tests__/ingredients.test.js
+++ src/__tests__/ingredients.test.js
@@ -1,22 +1,23 @@
 import * as actions from "../services/actions/ingredients";
 import * as types from "../services/constants/ingredients";
+import { ingredientsReducer } from "../services/reducers/ingredients";
+
+const ingredient = {
+  _id: "60d3b41abdacab0026a733c8",
+  name: "Филе Люминесцентного тетраодонтимформа",
+  type: "main",
+  proteins: 44,
+  fat: 26,
+  carbohydrates: 85,
+  calories: 643,
+  price: 988,
+  image: "https://code.s3.yandex.net/react/code/meat-03.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+  __v: 0,
+};
 
 describe("Action creators of ingredients", () => {
-  const ingredient = {
-    _id: "60d3b41abdacab0026a733c8",
-    name: "Филе Люминесцентного тетраодонтимформа",
-    type: "main",
-    proteins: 44,
-    fat: 26,
-    carbohydrates: 85,
-    calories: 643,
-    price: 988,
-    image: "https://code.s3.yandex.net/react/code/meat-03.png",
-    image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
-    image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
-    __v: 0,
-  };
-
   it("GET_INGREDIENTS_SUCCESS", () => {
     const ingredients = [ingredient];
     const expectedAction = {
@@ -32,5 +33,52 @@ describe("Action creators of ingredients", () => {
       ingredient,
     };
     expect(actions.setCurrentIngredient(ingredient)).toEqual(expectedAction);
+  });
+});
+
+describe("ingredient reducer", () => {
+  const state = {
+    ingredients: [],
+    ingredientsRequest: false,
+    ingredientsFailed: false,
+    ingredientsSuccess: false,
+    currentIngredient: null,
+  };
+  it("should return this initial state", () => {
+    expect(ingredientsReducer(undefined, {})).toEqual(state);
+  });
+
+  it("should return state with CLEAR_CURRENT_INGREDIENT ", () => {
+    expect(
+      ingredientsReducer(
+        { ...state, currentIngredient: ingredient },
+        {
+          type: types.CLEAR_CURRENT_INGREDIENT,
+        },
+      ),
+    ).toEqual(state);
+  });
+
+  it("should return state with GET_INGREDIENTS_FAILED ", () => {
+    expect(
+      ingredientsReducer(undefined, {
+        type: types.GET_INGREDIENTS_FAILED,
+      }),
+    ).toEqual({ ...state, ingredientsFailed: true });
+  });
+  it("should return state with GET_INGREDIENTS_REQUEST ", () => {
+    expect(
+      ingredientsReducer(undefined, {
+        type: types.GET_INGREDIENTS_REQUEST,
+      }),
+    ).toEqual({ ...state, ingredientsRequest: true });
+  });
+  it("should return state with GET_INGREDIENTS_SUCCESS ", () => {
+    expect(
+      ingredientsReducer(undefined, {
+        type: types.GET_INGREDIENTS_SUCCESS,
+        ingredients: [ingredient],
+      }),
+    ).toEqual({ ...state, ingredientsSuccess: true, ingredients: [ingredient] });
   });
 });
