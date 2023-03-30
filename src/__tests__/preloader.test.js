e45d7aec847215bc39ed99f919f844a247cba522
roman <rm@indrive-tech.com>
2023-03-30 15:26:54 +0300

    test preloader reducer

--- src/__tests__/preloader.test.js
+++ src/__tests__/preloader.test.js
@@ -1,5 +1,6 @@
 import * as actions from "../services/actions/preloader";
 import * as types from "../services/constants/preloader";
+import { preloaderReducer } from "../services/reducers/preloader";
 
 describe("Action creators of preloader", () => {
   it("Show preloader", () => {
@@ -16,5 +17,30 @@ describe("Action creators of preloader", () => {
       type: types.HIDE_PRELOADER,
     };
     expect(actions.hidePreloader()).toEqual(expectedAction);
+  });
+});
+
+describe("preload reducer", () => {
+  const state = {
+    preloaderShow: false,
+  };
+  it("should return this initial state", () => {
+    expect(preloaderReducer(undefined, {})).toEqual(state);
+  });
+
+  it("should return state with HIDE_PRELOADER ", () => {
+    expect(
+      preloaderReducer(undefined, {
+        type: types.HIDE_PRELOADER,
+      }),
+    ).toEqual({ ...state, preloaderShow: false });
+  });
+
+  it("should return state with SHOW_PRELOADER ", () => {
+    expect(
+      preloaderReducer(undefined, {
+        type: types.SHOW_PRELOADER,
+      }),
+    ).toEqual({ ...state, preloaderShow: true });
   });
 });
